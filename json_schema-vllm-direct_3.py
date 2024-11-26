a26f4ef889fd98b84aa134dd9540ddd13d44bd28
Aidan Do <aidando73@gmail.com>
2024-11-26 06:39:48 +0000

    .

---
+++ json_schema-vllm-direct_3.py
@@ -0,0 +1,31 @@
+import json
+from openai import OpenAI
+
+client = OpenAI(base_url="http://localhost:8000/v1", api_key="fake")
+
+completion = client.chat.completions.create(
+    model="meta-llama/Llama-3.2-3B-Instruct",
+    messages=[
+        {
+            "role": "user",
+            "content": "Write me a print statement that prints 'Hello, World!' in python",
+        },
+    ],
+    extra_body={
+        "guided_json": {
+            "name": "name_of_response_format",
+            "schema": {
+                "type": "object",
+                "properties": {
+                    "completion_message": {
+                        "type": "string",
+                    }
+                },
+                "required": ["completion_message"],
+            },
+            "strict": True,
+        },
+    },
+)
+
+print(completion.choices[0].message.content)
