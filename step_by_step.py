9cea3dd5a86bd97cd71dd71b56dacadeb74de26e
Aidan Do <aidando73@gmail.com>
2024-11-26 07:39:42 +0000

    .

---
+++ step_by_step.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+import os
+from llama_stack_client import LlamaStackClient
+
+client = LlamaStackClient(base_url=f"http://localhost:{os.environ['LLAMA_STACK_PORT']}")
+
+class Explanation(BaseModel):
+    explanation: str
+
+class StepByStepCompletionResponse(BaseModel):
+    steps: list[Explanation]
+
+models = client.models.list()
+print(models)
+
+response = client.inference.chat_completion(
+    model_id=os.environ["INFERENCE_MODEL"],
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Give me a proof for the chain rule in calculus. Take me through it step by step."}
+    ],
+    response_format={
+      "type": "json_schema",
+      "json_schema": StepByStepCompletionResponse.model_schema_json()
+    }
+)
+print(response.completion_message.content)
