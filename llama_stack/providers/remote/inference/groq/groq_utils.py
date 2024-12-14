0056f6d9368f3201b6169941903b256926d783b0
Aidan Do <aidando73@gmail.com>
2024-12-14 19:10:55 +1100

    .

--- llama_stack/providers/remote/inference/groq/groq_utils.py
+++ llama_stack/providers/remote/inference/groq/groq_utils.py
@@ -214,7 +214,7 @@ async def convert_chat_completion_response_stream(
             stop_reason = _map_finish_reason_to_stop_reason(choice.finish_reason)
 
         if choice.delta.tool_calls:
-            # We assume there is only one tool call per chunk, but print out a warning in case we're wrong
+            # We assume there is only one tool call per chunk, but emit a warning in case we're wrong
             if len(choice.delta.tool_calls) > 1:
                 warnings.warn("Groq returned multiple tool calls in one chunk. Using the first one, ignoring the rest.")
 
